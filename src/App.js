9ddd9f375eabf80d71d342e5f64c83ad9b13d499
Issah Ousman <ousiota1@gmail.com>
2021-09-01 12:05:27 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Userstate, useState } from "react";
+import  Buttom from "./components/Buttom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  function App() {
+    const [email, setEmail] = useState("")
+    const [fisrtname, setfirstname] = useState("")
+    const [Lastname, setLastname] = useState("")
+    const [Address, setAddress] = useState("")
+  }
+
+   function handleEmail (e) {
+     setEmail.email.value
+   }
+   function Handlefirstname (e) {
+     setfisrtname.firstname.value
+   }
+   function Handlelastname (e) {
+     setlastname.lastname.value
+   }
+   function HandleAddress (e) {
+     setAddress.Address.value
+   }
+   function handlesubmit (e) {
+     e.preventdefault() 
+     //sign in user
+     setEmail
+     setlastname
+     setfisrtname
+     setAddress
+   }
+
+Return (
+
+
+  <div>
+    <forms>
+      <imput name ="email" type= 'email' value= {email} onchange= {(e) => HandleEmail(e)}/>
+      <imput name ="firstname" type = "firstname" value = {firstname} onchange={(e) => Handlefirstname (e)}/>
+      <imput name = "Lastname" type = "lastname" value = {lastname} onchange={(e) => Handlelastname (e)}/>
+      <imput name ="Address" type ="Address" value = {Address} onchange= {(e) => HandleAddress (e)}/>
+      <imput type ="submit"/>
+    </forms>
+  </div>
+)
 }
 
+
 export default App;
+S
